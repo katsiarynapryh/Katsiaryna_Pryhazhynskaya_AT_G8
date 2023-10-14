acdc5a52651faa89e1489cbdf529356d67fe1b36
kpryhazhynskaya <kpryhazhynskaya@servicechannel.com>
2023-10-14 03:45:18 +0300

    clean repo

--- src/pretask/MessageExample.java
+++
@@ -1,7 +0,0 @@
-package pretask;
-
-public class MessageExample {
-    public static void main(String[] args) {
-        System.out.println("You've done it twice!");
-    }
-}
--- src/pretask/VariableAssignment.java
+++
@@ -1,9 +0,0 @@
-package pretask;
-
-public class VariableAssignment {
-    static int c = 22;
-    public static void main(String[] args)
-    {
-        System.out.println(c);
-    }
-}
